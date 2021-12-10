68952f0568ae1b867e67382cf56eb8b5f899e46b
HanJiHun <49916333+hahahohohun@users.noreply.github.com>
2021-12-10 15:44:25 +0900

    하이어라키에서 선택한 오브젝트를 제외한 모든 오브젝트 상태를 false 처리합니다.

---
+++ Tools/ECCustomTools.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ECCustomTools : MonoBehaviour
+{
+    [MenuItem("GameObject/IsolateActiveFalse %#q")]
+    public static void IsolateActiveFalse()
+    {
+        var list = Selection.GetTransforms(SelectionMode.TopLevel);
+
+        foreach (var item in list)
+        {
+            Transform traParent = item.parent;
+            if (traParent != null)
+            {
+                for (int i = 0; i < traParent.childCount; i++)
+                {
+                    if (item == traParent.GetChild(i))
+                        continue;
+
+                    Undo.RegisterCompleteObjectUndo(traParent.GetChild(i).gameObject, "GameObject/IsolateActiveFalse");
+                    traParent.GetChild(i).gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                GameObject[] all_Objs = SceneManager.GetActiveScene().GetRootGameObjects();
+                foreach (GameObject g in all_Objs)
+                {
+                    bool bSame = false;
+                    foreach (var cur in list)
+                    {
+                        if (cur == g.transform)
+                        {
+                            bSame = true;
+                            break;
+                        }
+                    }
+                    if (!bSame)
+                    {
+                        Undo.RegisterCompleteObjectUndo(g, "GameObject/IsolateActiveFalse");
+                        g.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+}
