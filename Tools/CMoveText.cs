1416c66bd6a344f5b5f0114586bbd649e12299d0
HanJiHun <49916333+hahahohohun@users.noreply.github.com>
2021-12-22 16:34:40 +0900

    흐르는 텍스트

---
+++ Tools/CMoveText.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CMoveText : MonoBehaviour
+{
+    public RectTransform ins_traTitle = null;
+    [Header("이동 속도")]
+    [SerializeField]
+    private int ins_nMoveSpeed = 150;
+    [Header("이동 방향")]
+    [SerializeField]
+    private bool ins_bRight = true;
+
+    //
+    private RectTransform _rtaBg;
+    private Vector2 _vStartPos;
+    private Vector2 _vDirection = Vector2.right;
+    private float _fEndPosX;
+
+    private void Start()
+    {
+        _rtaBg = transform.GetComponent<RectTransform>();
+
+        //ins_traTitle 가변 사이즈일 수 있기 때문에.
+        LayoutRebuilder.ForceRebuildLayoutImmediate(ins_traTitle);
+        float _fTexthalf = ins_traTitle.rect.width / 2 + (_rtaBg.rect.width / 2);
+        _fEndPosX = ins_traTitle.anchoredPosition.x;
+
+        if (ins_bRight)
+        {
+            _vDirection = Vector2.right;
+            _fEndPosX += _fTexthalf;
+        }
+        else
+        {
+            _vDirection = Vector2.left;
+            _fEndPosX -= _fTexthalf;
+        }
+        _vStartPos = new Vector2(-_fEndPosX, ins_traTitle.anchoredPosition.y);
+        ins_traTitle.anchoredPosition = _vStartPos;
+
+        StartCoroutine(CorMoveText());
+    }
+    private IEnumerator CorMoveText()
+    {
+        while (true)
+        {
+            ins_traTitle.Translate(_vDirection * ins_nMoveSpeed * Time.unscaledDeltaTime);
+            if (IsEndPos())
+            {
+                ins_traTitle.anchoredPosition = _vStartPos;
+            }
+            yield return null;
+        }
+    }
+
+    private bool IsEndPos()
+    {
+        if (ins_bRight)
+            return _fEndPosX < ins_traTitle.anchoredPosition.x;
+        else
+            return _fEndPosX > ins_traTitle.anchoredPosition.x;
+    }
+
+}
